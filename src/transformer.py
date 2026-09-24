38328ae20e8973061471860b4ca5267e4719be7a
0130 <lovenvibest@gmail.com>
2026-09-24 17:12:04 +0800

    fix(transformer): mask of encoder & decoder

--- src/transformer.py
+++ src/transformer.py
@@ -46,12 +46,12 @@ class MultiHeadAttention(nn.Module):
         self.v_proj = nn.Linear(d_model, d_model)
         self.out_proj = nn.Linear(d_model, d_model)  # get information from each head
 
-    def forward(self, x, ctx=None):  # [batch_size, seq_len, d_model]
+    def forward(self, x, ctx=None, mask=None):  # [batch_size, seq_len, d_model]
         """
-        x: [batch_size, seq_len, d_model]
-        ctx: [batch_size, src_len, d_model]
+        x: [batch_size, q_len, d_model]
+        ctx: [batch_size, kv_len, d_model]
         """
-        if ctx is None:  # encode stage
+        if ctx is None:
             ctx = x
         batch_size, q_len, _ = x.shape
         kv_len = ctx.size(1)
@@ -69,6 +69,8 @@ class MultiHeadAttention(nn.Module):
         ).transpose(1, 2)
         scores = q @ k.transpose(-1, -2)  # [batch_size, head_num, q_len, kv_len]
         scores = scores / math.sqrt(self.head_dim)
+        if mask is not None:
+            scores = scores.masked_fill(mask, float("-inf"))
         weights = torch.softmax(scores, -1)
         out = (
             (weights @ v).transpose(1, 2).reshape(batch_size, q_len, self.d_model)
@@ -111,8 +113,10 @@ class Encoder(nn.Module):
         self.ffn = FeedForwardLayer(d_model, head_num, 4 * d_model)
         self.conv2 = ResidualLayer(d_model)
 
-    def forward(self, x):  # [batch_size, seq_len, d_model]
-        x = self.conv1(self.attention(x), x)  # [batch_size, seq_len, d_model]
+    def forward(self, x, mask=None):  # [batch_size, seq_len, d_model]
+        x = self.conv1(
+            self.attention(x, mask=mask), x
+        )  # [batch_size, seq_len, d_model]
         x = self.conv2(self.ffn(x), x)
         return x
 
@@ -124,12 +128,15 @@ class Decoder(nn.Module):
         self.conv1 = ResidualLayer(d_model)
         self.encode_decode_attention = MultiHeadAttention(d_model, head_num)
         self.conv2 = ResidualLayer(d_model)
+
         self.ffn = FeedForwardLayer(d_model, head_num, 4 * d_model)
         self.conv3 = ResidualLayer(d_model)
 
-    def forward(self, x, memory):
-        x = self.conv1(self.attention(x), x)  # get information from generated output
-        x = self.conv2(self.encode_decode_attention(x, memory), x)
+    def forward(self, x, memory, tgt_mask=None, src_mask=None):
+        x = self.conv1(
+            self.attention(x, mask=tgt_mask), x
+        )  # get information from generated output
+        x = self.conv2(self.encode_decode_attention(x, ctx=memory, mask=src_mask), x)
         x = self.conv3(self.ffn(x), x)
         return x
 
@@ -150,7 +157,35 @@ class Transformer(nn.Module):
 
         self.embedding = nn.Embedding(self.vocab_size, self.d_model, padding_idx=0)
         self.positional_embedding = PositionalEmbedding(d_model, max_len)
+        self.encoder = Encoder(d_model, head_num)
+        self.decoder = Decoder(d_model, head_num)
+        self.out_proj = nn.Linear(d_model, vocab_size)
 
-    def forward(self, x: torch.Tensor):
-        out = self.embedding(x)  # [batch_size, seq_len, d_model]
-        out = self.positional_embedding(out)  # [batch_size, seq_len, d_model]
+    def forward(self, src_ids: torch.Tensor, tgt_ids: torch.Tensor):
+        """
+        src_ids: [batch_size, q_len]
+        tgt_ids: [batch_size, kv_len]
+        """
+        src_padding_mask = (src_ids == 0)[:, None, None, :]  # [batch_size, 1, 1, q_len]
+        tgt_padding_mask = (tgt_ids == 0)[
+            :, None, None, :
+        ]  # [batch_size, 1, 1, kv_len]
+        kv_len = tgt_ids.size(1)
+        tgt_causal_mask = torch.triu(
+            torch.ones(kv_len, kv_len, dtype=torch.bool, device=tgt_ids.device),
+            diagonal=1,  # diagonal = 1 not mask itself
+        )
+        src_mask = src_padding_mask  # [batch_size, 1, 1, q_len]
+        tgt_mask = tgt_padding_mask | tgt_causal_mask  # [batch_size, 1, q_len, kv_len]
+        src_embedding = self.embedding(src_ids)  # [batch_size, q_len, d_model]
+        tgt_embedding = self.embedding(tgt_ids)  # [batch_size, kv_len, d_model]
+        src_embedding = self.positional_embedding(
+            src_embedding
+        )  # [batch_size, q_len, d_model]
+        tgt_embedding = self.positional_embedding(
+            tgt_embedding
+        )  # [batch_size, kv_len, d_model]
+        src = self.encoder(src_embedding, mask=src_mask)
+        tgt = self.decoder(tgt_embedding, memory=src, mask=tgt_mask)
+        out = torch.softmax(self.out_proj(tgt), dim=-1, dtype=torch.float32)
+        return out
