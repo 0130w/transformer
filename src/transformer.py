fbea941051393c52f3d76ae12fbd9e54134e45e2
0130 <lovenvibest@gmail.com>
2026-09-24 15:12:25 +0800

    feat(MHA): support ctx argument In Decoder, the MHA layer need use the output of the last encoder

--- src/transformer.py
+++ src/transformer.py
@@ -46,26 +46,33 @@ class MultiHeadAttention(nn.Module):
         self.v_proj = nn.Linear(d_model, d_model)
         self.out_proj = nn.Linear(d_model, d_model)  # get information from each head
 
-    def forward(self, x):  # [batch_size, seq_len, d_model]
-        batch_size, seq_len, _ = x.shape
+    def forward(self, x, ctx=None):  # [batch_size, seq_len, d_model]
+        """
+        x: [batch_size, seq_len, d_model]
+        ctx: [batch_size, src_len, d_model]
+        """
+        if ctx is None:  # encode stage
+            ctx = x
+        batch_size, q_len, _ = x.shape
+        kv_len = ctx.shape(1)
         q = self.q_proj(x)
-        k = self.k_proj(x)
-        v = self.v_proj(x)
+        k = self.k_proj(ctx)
+        v = self.v_proj(ctx)
         q = torch.reshape(
-            q, (batch_size, seq_len, self.head_num, self.head_dim)
-        ).transpose(1, 2)  # [batch_size, head_num, seq_len, head_dim]
+            q, (batch_size, q_len, self.head_num, self.head_dim)
+        ).transpose(1, 2)  # [batch_size, head_num, q_len, head_dim]
         k = torch.reshape(
-            k, (batch_size, seq_len, self.head_num, self.head_dim)
-        ).transpose(1, 2)
+            k, (batch_size, kv_len, self.head_num, self.head_dim)
+        ).transpose(1, 2)  # [batch_size, head_num, kv_len, head_dim]
         v = torch.reshape(
-            v, (batch_size, seq_len, self.head_num, self.head_dim)
+            v, (batch_size, kv_len, self.head_num, self.head_dim)
         ).transpose(1, 2)
-        scores = q @ k.transpose(-1, -2)  # [batch_size, head_num, seq_len, seq_len]
+        scores = q @ k.transpose(-1, -2)  # [batch_size, head_num, q_len, kv_len]
         scores = scores / math.sqrt(self.head_dim)
         weights = torch.softmax(scores, -1)
         out = (
-            (weights @ v).transpose(1, 2).reshape(batch_size, seq_len, self.d_model)
-        )  # [batch_size, seq_len, d_model]
+            (weights @ v).transpose(1, 2).reshape(batch_size, q_len, self.d_model)
+        )  # [batch_size, q_len, d_model]
         out = self.out_proj(out)
         return out
 
@@ -118,10 +125,18 @@ class Decoder(nn.Module):
         self.attention = MultiHeadAttention(d_model, head_num)
         self.conv1 = ResidualLayer(d_model)
         self.encode_decode_attention = MultiHeadAttention(d_model, head_num)
+        self.conv2 = ResidualLayer(d_model)
+        self.ffn = FeedForwardLayer(d_model, head_num, 4 * d_model)
+        self.conv3 = ResidualLayer(d_model)
 
-    def forward(self, x):
+    def forward(self, x, memory):
         out = self.attention(x)  # get information from generated output
         out = self.conv1(out, x)
+        out = self.encode_decode_attention(x, memory)
+        out = self.conv2(out, x)
+        out = self.ffn(out)
+        out = self.conv3(out)
+        return out
 
 
 class Transformer(nn.Module):
